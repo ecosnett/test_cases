bcd92c0afff5ee1c05904ade9579cb6b0a1b38ed
ecosnett <157140192+ecosnett@users.noreply.github.com>
2024-04-12 15:39:29 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+class tests(unittest.TestCase):
+    def test_div(self):
+        assert get_div(search) == str(ws["S"+str(count)].value)
+        assert get_div(" ") == None
+
+    def test_total(self):
+        assert get_total() == str(ws["S9"].value)
+
+if __name__=='__main__':
+    unittest.main()
